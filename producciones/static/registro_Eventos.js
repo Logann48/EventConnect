990f21ff0578fa53acc70448415821066c54f4b4
cbastdas1 <140538449+cbastdas1@users.noreply.github.com>
2023-09-24 18:43:24 -0400

    Cambios a RegistroEvento.html
    algunas modificaciones menores para las validaciones, aun no están listas del todo

--- producciones/static/registro_Eventos.js
+++ producciones/static/registro_Eventos.js
@@ -2,9 +2,11 @@ const formulario = document.getElementById('formulario');
 const inputs = document.querySelectorAll('#formulario input, #formulario textarea');
 
 const expresiones = {
-	nombreEv: /^[a-zA-ZÀ-ÿ\s]{1,100}$/, 
+	nombreEv: /^[a-zA-ZÀ-ÿ0-9\s]{1,100}$/, // Permitir números
 	horaEv: /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/,
-	descripEv: /^[a-zA-Z0-9\s\.\,\;\:\-]{1,150}$/,
+	descripEv: /^[a-zA-Z0-9\s\.\,\;\:\-ñÑ]{1,150}$/, // Permitir la letra "ñ"
+	tipo: /^[a-zA-Z0-9]{1,15}$/,
+	entrada: /^\d+(\.\d{1,2})?$/
 }
 
 const campos = {
@@ -24,17 +26,35 @@ const validarFormulario = (e) => {
 		case "descripEv":
 			validarCampo(expresiones.descripEv, e.target, 'descripEv');
 		break;
+		case "myDate":
+			validarFecha(e.target, 'myDate');
+		break;
+		default:
+			// Para los campos "tipo" y "entrada", iteramos a través de cada uno
+			for (let i = 1; i <= 4; i++) {
+				if (e.target.name === "tipo" && e.target.id === `tipo${i}`) {
+					validarCampo(expresiones.tipo, e.target, `tipo${i}`);
+				}
+				if (e.target.name === "entrada" && e.target.id === `entrada${i}`) {
+					validarCampo(expresiones.entrada, e.target, `entrada${i}`);
+				}
+			}
+		break;
 	}
 }
 
 const validarCampo = (expresion, input, campo) => {
-	if(expresion.test(input.value)){
-		document.getElementById(`${campo}`).classList.remove('nombre_incorrecto');
-		document.getElementById(`${campo}`).classList.add('nombre_correcto');
+	if (input.value.trim() === '') {
+		input.classList.remove('nombre_correcto');
+		input.classList.add('nombre_incorrecto');
+		campos[campo] = false;
+	} else if (expresion.test(input.value)) {
+		input.classList.remove('nombre_incorrecto');
+		input.classList.add('nombre_correcto');
 		campos[campo] = true;
 	} else {
-		document.getElementById(`${campo}`).classList.add('nombre_incorrecto');
-		document.getElementById(`${campo}`).classList.remove('nombre_correcto');
+		input.classList.remove('nombre_correcto');
+		input.classList.add('nombre_incorrecto');
 		campos[campo] = false;
 	}
 }
@@ -60,12 +80,21 @@ inputs.forEach((input) => {
 });
 
 formulario.addEventListener('submit', (e) => {
-	if(!(campos.nombreEv && campos.horaEv && campos.descripEv)){
-		e.preventDefault();
+
+	let allValid = Object.values(campos).every((campo) => campo === true);
+	for (let i = 1; i <= 4; i++) {
+		allValid = allValid && document.getElementById(`tipo${i}`).classList.contains('nombre_correcto');
+		allValid = allValid && document.getElementById(`entrada${i}`).classList.contains('nombre_correcto');
+		allValid = allValid && document.getElementById(`tipo${i}`).value !== '';
+		allValid = allValid && document.getElementById(`entrada${i}`).value !== '';
 	}
-	else{
+	if (!allValid) {
+		e.preventDefault();
+		alert('Por favor, corrija los campos marcados en rojo antes de enviar el formulario.');
+	} else {
 		document.querySelectorAll('.nombre_correcto').forEach((icono) => {
 			icono.classList.remove('nombre_correcto');
 		});
 	}
 });
+
